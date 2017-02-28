15473b9e37029fdf781cd250ac3d3ccd770997e0
Kerick Howlett <kerick@dbswebsite.com>
2017-02-28 16:23:24 -0500

    First commit.

---
+++ breadcrumb.js
@@ -0,0 +1,21 @@
+/**
+ * This JavaScript/jQuery file is to URI decodes and removes all of the special
+ * characters from the breadcrumbs, as the breadcrumbs are created from the
+ * page's URI.
+ *
+ * @author Kerick Howlett
+ **/
+
+( function( $ ) {
+	$( document ).ready( function() {
+		$.each( $( '.breadcrumb__link a' ), function( index, value ) {
+			var breadcrumb = $( value ).html();
+			if ( breadcrumb !== 'Home' ) {
+				breadcrumb = decodeURI( breadcrumb );
+				breadcrumb = breadcrumb.replace( /[^a-zA-Z0-9 ]/g,' ' );
+				breadcrumb = breadcrumb.replace( /\s\s+/g, ' ' );
+				$( value ).html( breadcrumb );
+			}
+		} );
+	} );
+} )( jQuery );
